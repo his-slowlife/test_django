d83b351938c237fec1a0ae3ccc62f286433aa9be
Serzh <serzh.goryachev.2014@gmail.com>
2020-12-18 20:12:12 +0300

    Deploy

--- test_django/settings.py
+++ test_django/settings.py
@@ -12,16 +12,12 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os, sys
-import dj-database-url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 PROJECT_ROOT = os.path.dirname(__file__)
 sys.path.insert(0, os.path.join(PROJECT_ROOT, 'apps'))
-
-db_from_env = dj-database-url.config()
-DATABASE['default'].update(db_from_env)
 
 
 # Quick-start development settings - unsuitable for production
